603648619d977fbdb309109c3bed268fc62eb678
Vitor andes <vitor.andes.santos04@gmail.com>
2024-11-04 20:26:40 -0400

    ""

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import HeaderComponent from './components/HeaderComponent.vue';
-import InicioComponent from './components/InicioComponent.vue';
-import SobreComponent from './components/SobreComponent.vue';
-import ServicosComponent from './components/ServicosComponent.vue';
 import ContatoComponent from './components/ContatoComponent.vue';
 import divider from './components/divider.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import InicioComponent from './components/InicioComponent.vue';
+import ServicosComponent from './components/ServicosComponent.vue';
+import SobreComponent from './components/SobreComponent.vue';
 
 </script>
 
